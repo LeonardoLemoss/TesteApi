cbb504b276ba7898bb542191ac5d26e1424c2503
leonardo lemos <leoo_lemos@outlook.com>
2019-12-11 00:05:25 -0300

    fix erro de build

--- src/main/java/com/example/api/web/rest/EnderecoController.java
+++ src/main/java/com/example/api/web/rest/EnderecoController.java
@@ -3,16 +3,13 @@ package com.example.api.web.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.domain.CorreirosCep;
 import com.example.api.execption.InvalidCepExecption;
-import com.example.api.repository.EnderecoRepository;
-import com.example.api.service.CustomerService;
 import com.example.api.service.EnderecoService;
 
 @RestController
@@ -26,7 +23,7 @@ public class EnderecoController {
 		this.service = service;
 	}
 
-	@GetMapping(value="/getCep/{cep}")
+	@RequestMapping(value = "/getCep/{cep}", method = RequestMethod.GET)
 	public ResponseEntity<CorreirosCep> ReceberCep(@PathVariable(name = "cep") String cep) throws InvalidCepExecption{
 		
 		CorreirosCep response = service.buscarCep(cep);
